57eede8dc346a2ff7c2f56936d34cdfa14236ea5
Cameron Youell <cameronyouell@gmail.com>
2025-05-09 12:05:44 +1000

    Make it mine

---
+++ lua/plugins.lua
@@ -0,0 +1,28 @@
+--- === Plugins ===
+--- General idea, if more complicated than an import it has
+--- its own file in cammo1123/plugins
+
+require('lazy').setup({
+  'tpope/vim-sleuth',
+  { import = 'cammo1123.plugins' },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
